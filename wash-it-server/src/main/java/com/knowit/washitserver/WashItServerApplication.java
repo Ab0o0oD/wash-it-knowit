1d7d2b22f2510be4ff23f91937d92e10ca879806
Ab0o0oD <abdulrzzak.kanjo@gmail.com>
2021-11-04 20:28:25 +0100

    Create multi-module project with maven

---
+++ wash-it-server/src/main/java/com/knowit/washitserver/WashItServerApplication.java
@@ -0,0 +1,13 @@
+package com.knowit.washitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashItServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashItServerApplication.class, args);
+	}
+
+}
---
+++ wash-it-server/src/test/java/com/knowit/washitserver/WashItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowit.washitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
